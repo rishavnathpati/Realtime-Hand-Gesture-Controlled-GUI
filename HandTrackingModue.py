24ac0c4a1beaad2cd6e9a3f00229c4caac1d818c
rishavnathpati <patirishavnath@gmail.com>
2021-04-12 16:28:11 +0530

    Added findposition function

--- HandTrackingModue.py
+++ HandTrackingModue.py
@@ -36,6 +36,22 @@ class HandDetector():
                                                self.mpHands.HAND_CONNECTIONS)
         return img
 
+    def findPosition(self, img, handNo=0, draw=True):
+
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print(id, cx, cy)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        return lmList
+
 
 def main():
 
